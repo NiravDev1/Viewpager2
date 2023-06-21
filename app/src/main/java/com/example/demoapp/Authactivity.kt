4643b9768908f4d52207a095f6549481667e8258
Nirav <niravpanchal6268@gmail.com>
2023-06-21 23:08:36 +0530

    button manage

---
+++ app/src/main/java/com/example/demoapp/Authactivity.kt
@@ -0,0 +1,11 @@
+package com.example.demoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Authactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authactivity)
+    }
+}
